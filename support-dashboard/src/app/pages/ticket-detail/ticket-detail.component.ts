fb416b32a4db1857c978cf8e6cb9c2bbcc3e080c
mathias <mathias.detrez@gmail.com>
2026-09-08 10:48:47 +0200

    feat: page de detail d'un ticket

--- support-dashboard/src/app/pages/ticket-detail/ticket-detail.component.ts
+++ support-dashboard/src/app/pages/ticket-detail/ticket-detail.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Ticket } from '../../models/ticket.model';
+import { TicketService } from '../../services/ticket.service';
 
 @Component({
   selector: 'app-ticket-detail',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './ticket-detail.component.html',
-  styleUrl: './ticket-detail.component.css'
 })
-export class TicketDetailComponent {
+export class TicketDetailComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly ticketService = inject(TicketService);
 
+  ticket: Ticket | null = null;
+  error = '';
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.ticketService.getById(id).subscribe({
+      next: (ticket) => (this.ticket = ticket),
+      error: () => (this.error = 'Ticket introuvable.'),
+    });
+  }
 }
